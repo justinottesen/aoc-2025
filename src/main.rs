34732c880e878eb9536115cca7c00595e2df3b20
Justin Ottesen <justinottesen@gmail.com>
2025-12-02 23:52:49 -0500

    Initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let usage = "Usage: aoc-2025 <day> [part]";
+
+    let mut args = env::args();
+
+    let _ = args.next().expect("program name missing");
+
+    let day: u8 = args
+        .next()
+        .expect(usage)
+        .parse()
+        .expect("day must be a number");
+
+    let part: u8 = args
+        .next()
+        .expect(usage)
+        .parse()
+        .expect("part must be a number");
+
+    println!("Executing with day={day}, part={part}");
+
+    match (day, part) {
+        _ => println!("Invalid day / part provided"),
+    }
 }
