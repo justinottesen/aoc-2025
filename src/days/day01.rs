20cdcdb43342a6eca2ed0bb88c2985e4517dc78a
Justin Ottesen <justinottesen@gmail.com>
2025-12-06 13:12:16 -0500

    Day 1 Part 2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -3,7 +3,9 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-pub fn run_part1(input_path: String) {
+use crate::Part;
+
+pub fn run(input_path: String, part: Part) {
     let file = File::open(input_path).expect("Unable to open file");
 
     let reader = BufReader::new(file);
@@ -12,21 +14,50 @@ pub fn run_part1(input_path: String) {
 
     let mut val = 50;
     for line_result in reader.lines() {
+        let start = val;
         let line = line_result.expect("Unable to parse line");
+
+        println!("Val is {val}, readling line {line}");
 
         let (dir_str, diff_str) = line.split_at(1);
 
         let dir: char = dir_str.parse().expect("Not a valid char");
         let diff: i32 = diff_str.parse().expect("not a valid integer");
 
+        if part == Part::Two {
+            // Count full rotations to get it between [0, 100)
+            let rotations = diff / 100;
+            if rotations > 0 {
+                println!("More than a full rotation, incrementing code {code} {rotations}");
+                code += rotations;
+            }
+        }
+
+        // Perform rotation with remaining diff
+        let rem_diff = diff % 100;
         match dir {
-            'L' => val -= diff,
-            'R' => val += diff,
+            'L' => val -= rem_diff,
+            'R' => val += rem_diff,
             _ => unreachable!("Inavlid direction found: {dir} (from line {line})"),
         }
-        val %= 100;
 
+        let prev = val;
+        val %= 100;
+        if val < 0 {
+            val += 100;
+        }
+
+        if part == Part::Two {
+            // If different before / after mod, we passed 0 (and didn't end on it)
+            if prev != val && val != 0 && start != 0 {
+                println!("We passed 0, incrementing code {code} + 1");
+                code += 1;
+            }
+        }
+
+        // If we end on 0, increment
         if val == 0 {
+            println!("Ends on a 0, incrementing code {code} + 1");
             code += 1;
         }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
 mod days;
 
 pub fn run(day: u8, part: u8, input_path: String) {
-    match (day, part) {
-        (1, 1) => days::day01::run_part1(input_path),
+    let part_enum = match part {
+        1 => Part::One,
+        2 => Part::Two,
+        _ => unreachable!("Encountered unexpected part number: {part}"),
+    };
+    match day {
+        1 => days::day01::run(input_path, part_enum),
         _ => eprintln!("Day {day}, part {part} not implemented"),
     }
+}
+
+#[derive(PartialEq)]
+pub enum Part {
+    One,
+    Two,
 }
