c1e732ac50743d9c9a6c2348b904d188ff380bfb
Justin Ottesen <justinottesen@gmail.com>
2025-12-06 14:24:32 -0500

    Day 2 Part 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -6,20 +6,32 @@ pub fn run(input_path: &str, part: Part) {
     let mut answer: u64 = 0;
     for range in contents.split(',') {
         let (low_str, high_str) = range.split_once('-').expect("Invalid range");
-        println!("Low: {low_str}, High: {high_str}");
 
         let low: u64 = low_str.trim().parse().expect("Not a valid number");
         let high: u64 = high_str.trim().parse().expect("Not a valid number");
 
         for num in low..=high {
             let num_string = num.to_string();
-            if num_string.len() % 2 != 0 {
-                continue;
-            }
+            let bytes = num_string.as_bytes();
 
-            let (front, back) = num_string.split_at(num_string.len() / 2);
-            if front == back {
-                answer += num;
+            let max_num_chunks = match part {
+                Part::One => 2,
+                Part::Two => bytes.len(),
+            };
+
+            for num_chunks in 2..=max_num_chunks {
+                if bytes.len() % num_chunks != 0 {
+                    continue;
+                }
+
+                let chunk_len = bytes.len() / num_chunks;
+
+                let first = &bytes[..chunk_len];
+                if bytes.chunks(chunk_len).all(|chunk| chunk == first) {
+                    println!("Invalid: {num}");
+                    answer += num;
+                    break;
+                }
             }
         }
     }
