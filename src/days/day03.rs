d514cb8b29e38b004665dec5579b128fd45e3601
Justin Ottesen <justinottesen@gmail.com>
2025-12-06 17:25:32 -0500

    Day 3 Part 2

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -6,37 +6,46 @@ use std::{
 use crate::Part;
 
 pub fn run(input_path: &str, part: Part) {
+    let num_batteries = match part {
+        Part::One => 2,
+        Part::Two => 12,
+    };
+
     let file = File::open(input_path).expect("Unable to open file");
 
     let reader = BufReader::new(file);
 
-    let mut answer = 0;
+    let mut answer: u64 = 0;
     for line_result in reader.lines() {
         let line = line_result.expect("Unable to parse line");
         let line = line.trim();
 
-        let mut line_itr = line.chars().rev();
+        let joltage_str_len = line.len() - num_batteries;
 
-        let mut ls = line_itr.next().expect("Line too short");
-        let mut ms = line_itr.next().expect("Line too short");
+        let mut joltage: Vec<u8> = line[joltage_str_len..].bytes().collect();
+
+        let line_itr = line[..joltage_str_len].bytes().rev();
 
         for digit in line_itr {
-            if digit >= ms {
-                if ls <= ms {
-                    ls = ms;
-                }
-                ms = digit;
-            }
+            update_joltage(digit, &mut joltage[..]);
         }
 
-        let ls_value = ls.to_digit(10).expect("Couldn't convert char to digit");
-        let ms_value = ms.to_digit(10).expect("Couldn't convert char to digit");
+        let joltage_str = std::str::from_utf8(&joltage).expect("Non-UTF8 digits in joltage");
+        println!("Line: {line}, value {joltage_str}");
 
-        let value = ms_value * 10 + ls_value;
-        answer += value;
-
-        println!("Line: {line}, value {value}");
+        answer += joltage_str.parse::<u64>().expect("Unable to parse joltage");
     }
 
     println!("Answer: {answer}")
+}
+
+pub fn update_joltage(digit: u8, joltage_str: &mut [u8]) {
+    if joltage_str.len() == 0 {
+        return;
+    }
+
+    if digit >= joltage_str[0] {
+        update_joltage(joltage_str[0], &mut joltage_str[1..]);
+        joltage_str[0] = digit;
+    }
 }
