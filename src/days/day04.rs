c8d32478fef5bdd76c0721c86f1a5065b09006fb
Justin Ottesen <justinottesen@gmail.com>
2025-12-07 09:43:04 -0500

    Day 4 Part 2

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -20,7 +20,7 @@ pub fn run(input_path: &str, part: Part) {
     let file = File::open(input_path).expect("Unable to open file");
     let reader = BufReader::new(file);
 
-    let grid = {
+    let mut grid = {
         let mut mut_vec: Vec<Vec<u8>> = Vec::new();
         for line_result in reader.lines() {
             mut_vec.push(Vec::from(line_result.expect("Unable to parse line").trim()));
@@ -29,32 +29,49 @@ pub fn run(input_path: &str, part: Part) {
     };
 
     let mut answer: u32 = 0;
-    for (y, row) in grid.iter().enumerate() {
-        for (x, cell) in row.iter().enumerate() {
-            if *cell == b'.' {
-                print!(".");
-                continue;
-            }
-            let mut neighbors = 0;
-            for (dx, dy) in NEIGHBOR_DELTAS {
-                let (ny, overflowy) = y.overflowing_add_signed(dy);
-                let (nx, overflowx) = x.overflowing_add_signed(dx);
-                if overflowx || overflowy || ny >= grid.len() || nx >= row.len() {
+    loop {
+        let mut new_grid: Vec<Vec<u8>> = Vec::new();
+        let mut rolls_removed: u32 = 0;
+        for (y, row) in grid.iter().enumerate() {
+            let mut new_row: Vec<u8> = Vec::new();
+            for (x, cell) in row.iter().enumerate() {
+                if *cell != b'@' {
+                    new_row.push(b'.');
                     continue;
                 }
-                if grid[ny][nx] == b'@' {
-                    neighbors += 1;
+                let mut neighbors = 0;
+                for (dx, dy) in NEIGHBOR_DELTAS {
+                    let (ny, overflowy) = y.overflowing_add_signed(dy);
+                    let (nx, overflowx) = x.overflowing_add_signed(dx);
+                    if overflowx || overflowy || ny >= grid.len() || nx >= row.len() {
+                        continue;
+                    }
+                    if grid[ny][nx] == b'@' {
+                        neighbors += 1;
+                    }
+                }
+
+                if neighbors < 4 {
+                    new_row.push(b'x');
+                    rolls_removed += 1;
+                } else {
+                    new_row.push(b'@');
                 }
             }
-
-            if neighbors < 4 {
-                print!("x");
-                answer += 1;
-            } else {
-                print!("@");
-            }
+            new_grid.push(new_row);
         }
-        print!("\n");
+
+        if rolls_removed == 0 {
+            break;
+        } else {
+            answer += rolls_removed;
+        }
+
+        if part == Part::One {
+            break;
+        }
+
+        grid = new_grid;
     }
 
     println!("Answer: {answer}");
